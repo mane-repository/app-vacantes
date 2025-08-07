6bed0ed58d15eb56ad1377e12216a72ff21c61a4
Manuel Gomez <manuel.gomez@cecyteo.edu.mx>
2025-08-07 15:34:40 -0600

    Aplicación Vacantes

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -7,3 +7,11 @@ main_bp = Blueprint('main', __name__)
 def index():
     vacantes = Vacante.query.order_by(Vacante.fecha_publicacion.desc()).limit(3).all()
     return render_template('index.html', vacantes=vacantes)
+
+@main_bp.route('/acerca')
+def acerca():
+    return render_template('acerca.html')
+
+@main_bp.route('/detalle/<int:id>')
+def detalle():
+    return render_template('detalle.html')
